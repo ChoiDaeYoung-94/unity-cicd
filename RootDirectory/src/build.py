e2b7cf921ba6ee6d6bb26f330e6765f0877db228
ChoiDaeYoung-94 <chleodud1410@gmail.com>
2023-01-28 18:52:50 +0900

    CLI 개선 - test 필요 #27

--- RootDirectory/src/build.py
+++ RootDirectory/src/build.py
@@ -9,19 +9,19 @@ import git
 @click.option("--project_name", prompt="Enter github project repository name", help="github project repository name (ex: unity-cicd)")
 @click.option("--branch", prompt="Enter branch name", help="branch name (ex: main)")
 @click.option("--unity_app_path", prompt="Enter unity app path", help="https://docs.unity3d.com/kr/2021.3/Manual/EditorCommandLineArguments.html")
-@click.option("--buildpc_projects_path", prompt="Enter projects path of buildpc", help="unity projects path in buildpc")
-def main(user_name, project_name, branch, unity_app_path, buildpc_projects_path):
+@click.option("--buildpc_project_destination_path", prompt="Enter project destination path of buildpc", help="unity project destination path in buildpc")
+def main(user_name, project_name, branch, unity_app_path, buildpc_project_destination_path):
     global project_url
     global project_path
     global unity_path
 
     project_url = f"https://github.com/{user_name}/{project_name}.git"
-    project_path = f"{buildpc_projects_path}/{project_name}"
+    project_path = f"{buildpc_project_destination_path}/{project_name}"
     unity_path = unity_app_path
 
     if os.path.isdir(project_path) == False:
-        click.echo(f"there is no project > clone {project_url}")
-        git.Git(buildpc_projects_path).clone(project_url)
+        click.echo(f"there is no project in {buildpc_project_destination_path} > clone {project_url}")
+        git.Git(buildpc_project_destination_path).clone(project_url)
 
     click.echo(f"checkout {branch}")
     git.Git(project_path).checkout(branch)
@@ -36,12 +36,36 @@ def build():
     if os.path.isdir(f"{project_path}/Build"):
         shutil.rmtree(f"{project_path}/Build")
     os.mkdir(f"{project_path}/Build")
-
-    click.echo("Build AAB")
-    os.system(f"{unity_path} -buildTarget Android -projectPath {project_path} -executeMethod BuildScript.BuildAOSAAB")
     
-    click.echo("Build APK")
-    os.system(f"{unity_path} -buildTarget Android -projectPath {project_path} -executeMethod BuildScript.BuildAOSAPK")
+    click.echo("Create build log file")
+    buildLogPath = f"{project_path}/Build/buildLog.txt"
+    buildLogfile = open(buildLogPath, 'w')
+    buildLogfile.close()
+
+    click.echo("Build AAB - wait for build!!!!!!!!!!!!!!")
+    os.system(f"{unity_path} -logFile {buildLogPath} -buildTarget Android -projectPath {project_path} -executeMethod BuildScript.BuildAOSAAB")
+    
+    output = os.listdir(f"{project_path}/Build/AOS")
+    output_aab = [file for file in output if file.endswith(".aab")]
+    if output_aab.count != 0:
+        click.echo("AAB build success")
+    else:
+        click.echo(f"AAB build failed\nPlease see this log file > {buildLogfile}")
+        return
+
+    click.echo("Build APK - wait for build!!!!!!!!!!!!!!")
+    os.system(f"{unity_path} -logFile {buildLogPath} -buildTarget Android -projectPath {project_path} -executeMethod BuildScript.BuildAOSAPK")
+    
+    output = os.listdir(f"{project_path}/Build/AOS")
+    output_aab = [file for file in output if file.endswith(".apk")]
+    if output_aab.count != 0:
+        click.echo("APK build success")
+    else:
+        click.echo(f"APK build failed\nPlease see this log file > {buildLogfile}")
+        return
+    
+    os.system(f"cat {buildLogPath}")
+    click.echo("AAB, APK build success")
 
 if __name__ == '__main__':
     main()
