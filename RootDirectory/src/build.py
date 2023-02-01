aec910bc3a7115e69cc6008ecd93785ddf18ba1b
ChoiDaeYoung-94 <ChoiDaeYoung-94@gmail.com>
2023-02-01 15:16:07 +0900

    BuildPC에서 사용할 경우 추출물을 받을 수 있도록 내용 추가 #29

--- RootDirectory/src/build.py
+++ RootDirectory/src/build.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 import click
+import paramiko
+import scp
 import git
 
 
@@ -10,14 +12,26 @@ import git
 @click.option("--branch", prompt="Enter branch name", help="branch name (ex: main)")
 @click.option("--unity_app_path", prompt="Enter unity app path", help="https://docs.unity3d.com/kr/2021.3/Manual/EditorCommandLineArguments.html")
 @click.option("--buildpc_project_destination_path", prompt="Enter project destination path of buildpc", help="unity project destination path in buildpc")
-def main(user_name, project_name, branch, unity_app_path, buildpc_project_destination_path):
+@click.option("--ip", prompt="Enter IPv4 of your pc", help="window > cmd > ipconfig, mac > terminal > ifconfig | grep inet")
+@click.option("--username", prompt="Enter username of your pc", help="window > cmd > whoami, mac > terminal > whoami")
+@click.option("--password", prompt="Enter password of your pc", help="password of your pc")
+@click.option("--path", prompt="Enter path to receive build output", help="path to receive build output")
+def main(user_name, project_name, branch, unity_app_path, buildpc_project_destination_path, ip, username, password, path):
     global project_url
     global project_path
     global unity_path
+    global local_ip
+    global local_username
+    global local_password
+    global local_buildpath
 
     project_url = f"https://github.com/{user_name}/{project_name}.git"
     project_path = f"{buildpc_project_destination_path}/{project_name}"
     unity_path = unity_app_path
+    local_ip = ip
+    local_username = username
+    local_password = password
+    local_buildpath = path
 
     if os.path.isdir(project_path) == False:
         click.echo(f"there is no project in {buildpc_project_destination_path} > clone {project_url}")
@@ -65,6 +79,18 @@ def build():
         return
 
     click.echo("AAB, APK build success")
+
+    click.echo("receive build output")
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=local_ip, username=local_username, password=local_password, port=22)
+    sftp_client = scp.SCPClient(ssh.get_transport())
+
+    sftp_client.put(f"{project_path}/Build", local_buildpath, recursive=True)
+    sftp_client.close()
+    ssh.close()
+    
+    click.echo("Build complete")
 
 
 if __name__ == '__main__':
